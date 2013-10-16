b7ea946c629f41eaf5a7e35c112472c6eaf2979b
Lee Richardson <github@leerichardson.com>
2013-10-16 19:19:42 -0400

    Added Katas #2 and #3

---
+++ CSharpKatas/K02-ExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using NUnit.Framework;
+
+//namespace CSharpKatas
+//{
+//    // todo #0: Uncomment everything (Ctrl-A, Ctrl-KU)
+//    // todo #1: Without modifying SomeoneElsesClass add a new class that makes the unit tests pass
+//    // todo #2: Refactor so that there are no loops or if statements.
+//    // todo #3: (see below)
+
+
+//    #region DoNotModify
+//    // DO NOT MODIFY THIS FILE FOR THIS CLASS FOR THIS KATA
+//    public class SomeoneElsesClass
+//    {
+//        public List<int> Numbers { get; set; }
+//    }
+//    #endregion
+
+//    [TestFixture]
+//    public class TestExtensionMethods
+//    {
+//        [Test]
+//        public void FindNumberOrDefault_NumberExists_ReturnIt()
+//        {
+//            var someoneElsesClass = new SomeoneElsesClass
+//            {
+//                Numbers = new List<int> { 1, 2, 3 }
+//            };
+//            Assert.AreEqual(1, someoneElsesClass.FindNumberOrDefault(1));
+//        }
+
+//        [Test]
+//        public void FindNumberOrDefault_NumberDoesNotExist_ReturnNull()
+//        {
+//            var someoneElsesClass = new SomeoneElsesClass
+//            {
+//                Numbers = new List<int> { 1, 2, 3 }
+//            };
+//            var v = someoneElsesClass.FindNumberOrDefault(4);
+//            Assert.IsNull(v);
+//        }
+
+//        [Test]
+//        public void FindNumberOrDefault_SomeoneElsesClassIsNull_CheckForNull()
+//        {
+//            // todo #3: What's bizarre about the following test? Why is there no error? Consider reverse engineering the code with a tool like Just Decompile to get the answer.
+//            SomeoneElsesClass someoneElsesClass = null;
+//            Assert.IsNull(someoneElsesClass.FindNumberOrDefault(4));
+//        }
+//    }
+//}
+
---
+++ CSharpKatas/K03-FizzBuzz.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CSharpKatas
+{
+    public class K03FizzBuzz
+    {
+        public string MapNumber(int i)
+        {
+            // todo #1: Implement FizzBuzz (http://en.wikipedia.org/wiki/Bizz_buzz) for a single input
+            return null;
+        }
+
+        public IEnumerable<string> DoFizzBuzzUpTo(int n)
+        {
+            // todo #2: implement a FizzBuzz generator without using any foreach or any if statements
+            // todo #3: Refactor to use a "Method Group"
+            return null;
+        }
+    }
+
+    [TestFixture]
+    public class FizzBuzz
+    {
+        [Test]
+        public void MapNumber_DivideByThree_Fizz()
+        {
+            var fizzBuzz = new K03FizzBuzz();
+            Assert.AreEqual("Fizz", fizzBuzz.MapNumber(3));
+        }
+
+        [Test]
+        public void MapNumber_DivideByFive_Fizz()
+        {
+            var fizzBuzz = new K03FizzBuzz();
+            Assert.AreEqual("Buzz", fizzBuzz.MapNumber(5));
+        }
+
+        [Test]
+        public void MapNumber_DivideByThreeAndFive_FizzBuzz()
+        {
+            var fizzBuzz = new K03FizzBuzz();
+            Assert.AreEqual("Fizz Buzz", fizzBuzz.MapNumber(15));
+        }
+
+        [Test]
+        public void MapNumber_DivideByNeitherThreeNorFive_Number()
+        {
+            var fizzBuzz = new K03FizzBuzz();
+            Assert.AreEqual("2", fizzBuzz.MapNumber(2));
+        }
+
+        [Test]
+        public void DoFizzBuzzUpTo()
+        {
+            var actual = new K03FizzBuzz().DoFizzBuzzUpTo(36).ToList();
+            var expeted = new[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Fizz Buzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "Fizz Buzz", "31", "32", "Fizz", "34", "Buzz", "Fizz" };
+            Assert.AreEqual(actual.Count(), expeted.Length);
+        }
+    }
+}
