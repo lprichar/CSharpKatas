9aac7d79577ad9aafd18d5102e32d1108ae4b4c6
Lee Richardson <github@leerichardson.com>
2013-11-09 15:20:18 -0500

    Refactored out common log parser initialization logic

--- CSharpKatas/K09-DependencyInjection.cs
+++ CSharpKatas/K09-DependencyInjection.cs
@@ -34,30 +34,36 @@ namespace CSharpKatas
     }
 
     [TestFixture]
+    [Ignore]
     public class TestDoStuff
     {
         [Test]
-        [Ignore]
         public void GetRowsWithinRange_ThereIsARowWithinRange_ItsMessageIsReturned()
         {
             // todo #0: Remove the Ignore attribute
-            // todo #1: Start to clean up this disaster by focusing on LogParser. Extract out its File IO logic into a separate class so doesn't violate the single responsiblity principle (http://en.wikipedia.org/wiki/Solid_(object-oriented_design)).
+            // todo #1: Start to clean this up by focusing on LogParser. Extract out its File IO logic into a separate class so doesn't violate the single responsiblity principle (http://en.wikipedia.org/wiki/Solid_(object-oriented_design)).
             // todo #2: Implement the Inversion of Control (IoC) pattern in LogParser so that you could potentially use a separate implementation for the File IO logic
             // todo #3: Refactor the unit tests so that they never interact with the file system. Do this by subclassing the new class you created in #1 and injecting that into LogParser.
             // todo #4: Now that you've done dependency injection manually, redo #3 with Moq.  Get it via nuget, read the examples in the documentation on their website.
-
-            File.WriteAllText("C:\\temp\\Log-Proj1.csv", "1/1/2013 5:00 PM,Begin logging");
-            var logParser = new LogParser();
+            
+            var logParser = GetAndInitLogParser("C:\\temp\\Log-Proj1.csv", "1/1/2013 5:00 PM,Begin logging");
             var actual = logParser.GetMessagesWithinRange("Proj1", new DateTime(2013, 1, 1), new DateTime(2013, 1, 2)).ToList();
             Assert.AreEqual(1, actual.Count);
             Assert.AreEqual("Begin logging", actual[0]);
         }
 
+        public LogParser GetAndInitLogParser(string logName, string logContents)
+        {
+            if (!string.IsNullOrEmpty(logName))
+                File.WriteAllText(logName, logContents);
+            var logParser = new LogParser();
+            return logParser;
+        }
+
         [Test]
         public void GetRowsWithinRange_ThereIsARowOutsideOfRange_NothingIsReturned()
         {
-            File.WriteAllText("C:\\temp\\Log-Proj2.csv", "1/1/2013 5:00 PM,User attempted to log in");
-            var logParser = new LogParser();
+            var logParser = GetAndInitLogParser("C:\\temp\\Log-Proj2.csv", "1/1/2013 5:00 PM,User attempted to log in");
             var actual = logParser.GetMessagesWithinRange("User attempted to log in", new DateTime(2013, 2, 1), new DateTime(2013, 2, 2)).ToList();
             Assert.AreEqual(0, actual.Count);
         }
@@ -65,8 +71,7 @@ namespace CSharpKatas
         [Test]
         public void GetRowsWithinRange_ProjectDoesNotExist_NothingIsReturned()
         {
-            File.Delete("C:\\temp\\Log-Proj3.csv");
-            var logParser = new LogParser();
+            var logParser = GetAndInitLogParser(null, null);
             var actual = logParser.GetMessagesWithinRange("Proj3", new DateTime(2013, 2, 1), new DateTime(2013, 2, 2)).ToList();
             Assert.AreEqual(0, actual.Count);
         }
