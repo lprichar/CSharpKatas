f1cb63ce027b2b3f8e4a1c45779d7caa4763d257
Lee Richardson <github@leerichardson.com>
2013-11-09 14:06:58 -0500

    Converted Katas #1 and #9 to VB

--- VbKatas/K01-BasicLinq.vb
+++ VbKatas/K01-BasicLinq.vb
@@ -1,3 +1,50 @@
-﻿Public Class BasicLinq
+﻿Imports NUnit.Framework
+
+Public Class Calculator
+    Public Function SumNumbers(start, count)
+        ' todo #1: Remove the Ignore attribute and get all the tests passing
+        ' todo #2: Refactor so there are no loops or if statements (this is the functional approach and is idiomatic for C#)
+        ' todo #3: Refactor to use the Aggregate() LINQ method (see 101 Linq Samples: http://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b)
+        Return 0
+    End Function
+End Class
+
+<TestFixture(), Ignore()> _
+Public Class Test
+    <Test()> _
+    Public Sub SumNumbers_StartAt100SumNext1_TotalIs100()
+        Dim kata = New Calculator()
+        Assert.AreEqual(100, kata.SumNumbers(100, 1))
+    End Sub
+
+    <Test()> _
+    Public Sub SumNumbers_StartAt100SumNext2_TotalIs201()
+        Dim kata = New Calculator()
+        Assert.AreEqual(201, kata.SumNumbers(100, 2))
+    End Sub
+
+    <Test()> _
+    Public Sub SumNumbers_StartAt100SumNext0_TotalIs0()
+        Dim kata = New Calculator()
+        Assert.AreEqual(0, kata.SumNumbers(100, 0))
+    End Sub
+
+    <Test()> _
+    Public Sub SumNumbers_StartAt0SumNext0_TotalIs0()
+        Dim kata = New Calculator()
+        Assert.AreEqual(0, kata.SumNumbers(0, 0))
+    End Sub
+
+    <Test()> _
+    Public Sub SumNumbers_StartAt0SumNext3_TotalIs3()
+        Dim kata = New Calculator()
+        Assert.AreEqual(3, kata.SumNumbers(0, 3))
+    End Sub
+
+    <Test()> _
+    Public Sub SumNumbers_StartAt0SumNext100_TotalIs5050()
+        Dim kata = New Calculator()
+        Assert.AreEqual(5050, kata.SumNumbers(1, 100))
+    End Sub
 
 End Class
---
+++ VbKatas/K09-DependencyInjection.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO
+Imports System.Linq
+Imports NUnit.Framework
+
+Namespace CSharpKatas
+    Public Class ProjectLog
+        Public Sub New(logLine As String)
+            Dim bits As String() = logLine.Split(",")
+            EntryDate = DateTime.Parse(bits(0))
+            Message = bits(1)
+        End Sub
+
+        Public Property EntryDate() As DateTime
+        Public Property Message As String
+    End Class
+
+    Public Class LogParser
+        Public Function GetMessagesWithinRange(projectName As String, startDate As DateTime, endDate As DateTime) As IEnumerable(Of String)
+            Dim fileName As String = String.Format("C:\\temp\\Log-{0}.csv", projectName)
+
+            If Not File.Exists(fileName) Then
+                Return Enumerable.Empty(Of String)()
+            End If
+
+            Dim allLines = File.ReadAllLines(fileName)
+            Return allLines _
+                .Select(Function(i) New ProjectLog(i)) _
+                .Where(Function(i) i.EntryDate > startDate And i.EntryDate < endDate) _
+                .Select(Function(i) i.Message)
+        End Function
+    End Class
+
+    <TestFixture(), Ignore()> _
+    Public Class Test
+        <Test()> _
+        Public Sub GetRowsWithinRange_ThereIsARowWithinRange_ItsMessageIsReturned()
+            ' todo #0: Remove the Ignore attribute
+            ' todo #1: Start to clean up this disaster by focusing on LogParser. Extract out its File IO logic into a separate class so doesn't violate the single responsiblity principle (http://en.wikipedia.org/wiki/Solid_(object-oriented_design)).
+            ' todo #2: Implement the Inversion of Control (IoC) pattern in LogParser so that you could potentially use a separate implementation for the File IO logic
+            ' todo #3: Refactor the unit tests so that they never interact with the file system. Do this by subclassing the new class you created in #1 and injecting that into LogParser.
+            ' todo #4: Now that you've done dependency injection manually, redo #3 with Moq.  Get it via nuget, read the examples in the documentation on their website.
+
+            File.WriteAllText("C:\\temp\\Log-Proj1.csv", "1/1/2013 5:00 PM,Begin logging")
+            Dim logParser = New LogParser()
+            Dim actual = logParser.GetMessagesWithinRange("Proj1", New DateTime(2013, 1, 1), New DateTime(2013, 1, 2)).ToList()
+            Assert.AreEqual(1, actual.Count)
+            Assert.AreEqual("Begin logging", actual(0))
+        End Sub
+
+        <Test()> _
+        Public Sub GetRowsWithinRange_ThereIsARowOutsideOfRange_NothingIsReturned()
+            File.WriteAllText("C:\\temp\\Log-Proj2.csv", "1/1/2013 5:00 PM,User attempted to log in")
+            Dim logParser = New LogParser()
+            Dim actual = logParser.GetMessagesWithinRange("User attempted to log in", New DateTime(2013, 2, 1), New DateTime(2013, 2, 2)).ToList()
+            Assert.AreEqual(0, actual.Count)
+        End Sub
+
+        <Test()> _
+        Public Sub GetRowsWithinRange_ProjectDoesNotExist_NothingIsReturned()
+            File.Delete("C:\\temp\\Log-Proj3.csv")
+            Dim logParser = New LogParser()
+            Dim actual = logParser.GetMessagesWithinRange("Proj3", New DateTime(2013, 2, 1), New DateTime(2013, 2, 2)).ToList()
+            Assert.AreEqual(0, actual.Count)
+        End Sub
+    End Class
+End Namespace
